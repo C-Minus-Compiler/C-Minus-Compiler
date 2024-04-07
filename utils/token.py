48e7d5c1c4f37ddd9c738537c229bbcf66446adf
amkamir82 <amk_amir82@yahoo.com>
2024-04-07 06:37:31 +0330

    add get_next_token without handling comment

--- utils/token.py
+++ utils/token.py
@@ -8,18 +8,3 @@ class TokenType(Enum):
     SYMBOL = "SYMBOL"
     COMMENT = "COMMENT"
     WHITESPACE = "WHITESPACE"
-
-
-class TokenRegex(Enum):
-    digit = r'^\d$',
-    alphabet = r'^[a-zA-Z]$',
-    keyword = r'^if|else|void|int|repeat|break|until|return$',
-    symbol = r'^;|:|\[|]|\(|\)|{|}|\+|-|\*|=|<|,$',
-    whitespace = r'^\n|\r|\t|\v|\f| $',
-    new_line = r'^\n$',
-    slash = r'^/$',
-    star = r'^\*$',
-    single_line_comment_starter = r'^//$',
-    multi_line_comment_starter = r'^/\*$',
-    multi_line_comment_finisher = r'^\*/$',
-    equal_sign = r'^=$'
