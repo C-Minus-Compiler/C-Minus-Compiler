ff96ca395539f2ad67dfe2ea89b2e9bed67cdb16
amkamir82 <amk_amir82@yahoo.com>
2024-04-07 06:51:46 +0330

    add get_next_token without handling comment

--- scanner.py
+++ scanner.py
@@ -35,7 +35,7 @@ class Scanner:
 
     def get_next_state(self):
         char = self.read_next_index_of_code()
-        next_state = self.dfa.next_state(self.current_state, char)
+        next_state = self.dfa.get_next_state(self.current_state, char)
         return char, next_state
 
     def find_token_type(self):
@@ -91,45 +91,6 @@ class Scanner:
                 self.prepare_new_line()
 
 
-# class DFA:
-#     start = 0
-#     current_state = 0
-#     states = (0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-#     accept_states = (2, 4, 5, 7, 8, 11, 14, 15)
-#     stared_states = (2, 4, 8)
-#
-#     # (state, "regex", "true means should be false means should not be", "next_state")
-#     transitions = ((0, r'[0-9]', True, 1),
-#                    (1, r'[0-9]', False, 2),
-#                    (0, r'[a-zA-Z]', True, 3),
-#                    (3, r'[a-zA-Z0-9]', True, 3),
-#                    (3, r'[a-zA-Z0-9]', False, 4),
-#                    (0, r';|:|,|\[|\]|\(|\)|\{|\}|\+|\-|\*|<', True, 5),
-#                    (0, r'=', True, 6),
-#                    (6, r'=', True, 7),
-#                    (6, r'=', False, 8),
-#                    (0, r'/', True, 9),
-#                    (9, r'\*', True, 12),
-#                    (12, r'\*', False, 12),
-#                    (12, r'\*', True, 13),
-#                    (13, r'\*|/', False, 12),
-#                    (13, r'\*', True, 13),
-#                    (13, r'/', True, 14),
-#                    (0, r' |\n|\r|\t|\v|\f', True, 15))
-#
-#     # returns true if there is a valid next state
-#     def next_state(self, c):
-#         # for trn in self.transitions:
-#         #     if self.current_state == trn[0] and (trn[2] if re.match(trn[1], c) != None else not trn[2]):
-#         #         self.current_state = trn[2]
-#         #         return True
-#         # return False
-#         pass
-#
-#     # returns: Bool, Bool (accept, stared)
-#     def accept(self):
-#         return self.current_state in self.accept_states, self.current_state in self.stared_states
-
 def is_number_regex(char):
     return re.match('[0-9]', char)
 
@@ -147,8 +108,46 @@ def is_whitespace_regex(char):
 
 
 class DFA:
+    start = 0
+    current_state = 0
+    states = (0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+    accept_states = (2, 4, 5, 7, 8, 11, 14, 15)
+    stared_states = (2, 4, 8)
+
+    # (state, "regex", "true means should be false means should not be", "next_state")
+    transitions = ((0, r'[0-9]', True, 1),
+                   (1, r'[0-9]', False, 2),
+                   (0, r'[a-zA-Z]', True, 3),
+                   (3, r'[a-zA-Z0-9]', True, 3),
+                   (3, r'[a-zA-Z0-9]', False, 4),
+                   (0, r';|:|,|\[|\]|\(|\)|\{|\}|\+|\-|\*|<', True, 5),
+                   (0, r'=', True, 6),
+                   (6, r'=', True, 7),
+                   (6, r'=', False, 8),
+                   (0, r'/', True, 9),
+                   (9, r'\*', True, 12),
+                   (12, r'\*', False, 12),
+                   (12, r'\*', True, 13),
+                   (13, r'\*|/', False, 12),
+                   (13, r'\*', True, 13),
+                   (13, r'/', True, 14),
+                   (0, r' |\n|\r|\t|\v|\f', True, 15))
+
+    # returns true if there is a valid next state
+    def next_state(self, c):
+        # for trn in self.transitions:
+        #     if self.current_state == trn[0] and (trn[2] if re.match(trn[1], c) != None else not trn[2]):
+        #         self.current_state = trn[2]
+        #         return True
+        # return False
+        pass
+
+    # returns: Bool, Bool (accept, stared)
+    def accept(self):
+        return self.current_state in self.accept_states, self.current_state in self.stared_states
+
     # states are: number, identifier, symbol, whitespaces
-    def next_state(self, current_state, char):
+    def get_next_state(self, current_state, char):
         match current_state:
             # from initial state, we can go to every states
             case "start_state":
